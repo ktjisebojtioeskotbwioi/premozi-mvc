24a8d21eb16137b1ea34ec6f9e30a6821e3c4bc9
ktjisebojtioeskotbwioi <nemlopottnevxd@gmail.com>
2025-02-27 11:27:58 +0100

    Add project files.

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using System.Text.Encodings.Web;
+
+namespace premozi.Controllers
+{
+    public class FilmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string hello(string name="", int  id=1)
+        {
+            if(name == "")
+            {
+                return HtmlEncoder.Default.Encode("Hiiii hiiii hellooo");
+            }
+            else
+            {
+                return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+            }
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace premozi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
